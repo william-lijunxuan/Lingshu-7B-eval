c40548367448943255e097c365fcacb07939c589
william <mr.william.li@foxmail.com>
2025-12-07 00:32:00 +0000

    dos2unix config

--- qwenvl/eval/shell/Hulu_Med/run_all_hulu.sh
+++ qwenvl/eval/shell/Hulu_Med/run_all_hulu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-bash shell/Hulu_Med/Hulu_Med_4B.sh
 bash shell/Hulu_Med/Hulu_Med_7B.sh
+bash shell/Hulu_Med/Hulu_Med_4B.sh
 bash shell/Hulu_Med/Hulu_Med_14B.sh
