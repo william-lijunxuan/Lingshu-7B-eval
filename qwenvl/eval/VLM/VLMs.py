1919b40a1b673a615b3ffbc8c46ec3a04eb873eb
william <mr.william.li@foxmail.com>
2026-03-06 17:19:02 +0000

    modify Hulu-Med-30A3  moe model config

--- qwenvl/eval/VLM/VLMs.py
+++ qwenvl/eval/VLM/VLMs.py
@@ -59,6 +59,7 @@ class Hulu_Med_7B:
 def init_llm(args):
     try:
         print("--------args.model_name:",args.model_name)
+        print("--------registered models:", VLMRegistry.list_models())
         model_class = VLMRegistry.get_model(args.model_name)
         return model_class(args.model_path, args)
     except ValueError as e:
