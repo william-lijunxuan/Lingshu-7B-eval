046c875b9ffb115cbef904d766440272e1986357
william <mr.william.li@foxmail.com>
2026-03-06 17:51:15 +0000

    modify Hulu-Med-30A3  moe model config

--- qwenvl/eval/VLM/Qwen3vl_moe/Qwen3vl_moe.py
+++ qwenvl/eval/VLM/Qwen3vl_moe/Qwen3vl_moe.py
@@ -7,7 +7,7 @@ class Qwen3vl_moe:
     def __init__(self, model_path, args):
         super().__init__()
         self.llm =  Qwen3VLMoeForConditionalGeneration.from_pretrained(
-            model_path,torch_dtype=torch.bfloat16, device_map="auto", attn_implementation="flash_attention_2",trust_remote_code=True)
+            model_path,torch_dtype=torch.bfloat16, device_map="balanced", attn_implementation="flash_attention_2",trust_remote_code=True)
         self.processor = AutoProcessor.from_pretrained(model_path,trust_remote_code=True,use_fast=True,fix_mistral_regex=True)
         # self.processor.save_pretrained(model_path)
         self.temperature = args.temperature
